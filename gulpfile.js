35961ee016c3e62f67b994b99a358e6a64ac5dd6
Marat Abdullin <dakota@brokenpipe.ru>
2014-12-14 19:44:57 +0300

    Bower package placeholder.

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var eslint = require('gulp-eslint');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+
+gulp.task('eslint', function() {
+    return gulp.src(['gulpfile.js', 'fetcha.js'])
+        .pipe(eslint({
+            rules: {
+                'quotes': [2, 'single'],
+                'no-shadow-restricted-names': 0,
+                'no-underscore-dangle': 0,
+                'no-use-before-define': [2, 'nofunc'],
+                'no-new': 0,
+                'new-cap': 0,
+                'no-multi-spaces': 0,
+                'comma-spacing': 0
+            },
+            env: {
+                'node': true,
+                'browser': true
+            }
+        }))
+        .pipe(eslint.format());
+});
+
+
+gulp.task('uglify', function() {
+    return gulp.src(['fetcha.js'])
+        .pipe(uglify({preserveComments: 'some'}))
+        .pipe(rename('fetcha.min.js'))
+        .pipe(gulp.dest('.'));
+});
+
+
+gulp.task('assert-version', function(err) {
+    var assertVersion = require('assert-version');
+
+    err(assertVersion({
+        'fetcha.js': '',
+        'bower.json': ''
+    }));
+});
+
+
+gulp.task('test', function() {
+});
+
+
+gulp.task('default', ['eslint', 'uglify', 'assert-version', 'test']);
